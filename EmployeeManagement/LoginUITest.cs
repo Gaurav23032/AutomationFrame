f3e3aee947ec460785022d6ed3afda50b564120f
gauravsk <gauravksagar2020@gmail.com>
2023-01-03 16:48:45 +0530

    updated

--- EmployeeManagement/LoginUITest.cs
+++ EmployeeManagement/LoginUITest.cs
@@ -5,20 +5,36 @@ namespace EmployeeManagement
 {
     public class LoginUITest
     {
+        IWebDriver driver;
+        [SetUp]
+
+        public void BeforeMethod()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+
+        }
+        [TearDown]
+        public void AfterMethod()
+        {
+            driver.Quit();
+        }
 
         [Test]
         public void ValidateTitleTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
-          
             string actualTitle = driver.Title;
             Assert.That(actualTitle, Is.EqualTo("OrangeHRM"));
 
-
-
+        }
+        [Test]
+        public void ValidatePlaceholderTest()
+        {
+            string actualUsernamePlaceholder = driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
+            
         }
     }
 }
