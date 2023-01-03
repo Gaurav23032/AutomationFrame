004d042a19d68255ec4431dac16cc33d13238cc3
gauravsk <gauravksagar2020@gmail.com>
2023-01-03 16:07:11 +0530

    Add project files.

---
+++ EmployeeManagement/LoginUITest.cs
@@ -0,0 +1,24 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EmployeeManagement
+{
+    public class LoginUITest
+    {
+
+        [Test]
+        public void ValidateTitleTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+          
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle, Is.EqualTo("OrangeHRM"));
+
+
+
+        }
+    }
+}
