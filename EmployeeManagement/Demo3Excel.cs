8f34e25b8ce3be29b85a2271d76c593af9da2f70
gauravsk <gauravksagar2020@gmail.com>
2023-01-05 17:06:06 +0530

    DemoExcelRead

---
+++ EmployeeManagement/Demo3Excel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace EmployeeManagement
+{
+    public class Demo3Excel
+    {
+        [Test]
+        public void DemoExcelRead()
+        {
+            XLWorkbook book = new XLWorkbook(@"C:\Users\gauravsk\Desktop\C Sharpe\AutomationFramework\EmployeeManagement\TestData\OrangeHRM_data.xlsx");
+            IXLWorksheet sheet = book.Worksheet("InvalidLoginTest");
+            IXLRange range = sheet.RangeUsed();
+            string value = range.Cell(2, 1).GetString();
+            Console.WriteLine(value);
+            book.Dispose();
+        }
+
+    }
+}
